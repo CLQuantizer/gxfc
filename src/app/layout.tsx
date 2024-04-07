a53bac555406978bc741cb63bd40093854f1967c
eziox <ezioxxk@gmail.com>
2024-04-07 21:28:26 +0100

    gogo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import "./globals.css"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-        <body>
+        <body className={cn("w-screen h-full bg-background font-sans antialiased overflow-x-hidden", fontSans.variable)}>
           <div id="root">{children}</div>
         </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+'use client';
+import { Button } from "@/components/ui/button";
+
+const handleClicked = (input:string) => {
+  alert(input);
+}
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col w-full mt-2 gap-2 ">
+      <Button className="w-full" onClick={async () => handleClicked("nothing")} >
+        Click me for nothing</Button>
+        <div className="grid grid-cols-2">
+          <img className="w-64" src="/hand.jpg" alt="hand" />
+          <img className="w-64" src="/gxfc.jpg" alt="hand" />
+        </div>
+        <Button className="w-full" onClick={async () => handleClicked("than kyou")} >
+          Click me to thank you</Button>
     </div>
   );
 }
