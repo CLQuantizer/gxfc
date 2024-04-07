b84804b76405cf71ec7ab62972620bd1ddd6a486
eziox <ezioxxk@gmail.com>
2024-04-07 22:27:53 +0100

    kljkl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,17 @@ const handleClicked = (input:string) => {
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full mt-2 gap-2 ">
-      <Button className="w-full" onClick={async () => handleClicked("nothing")} >
-        Click me for nothing</Button>
-        <div className="grid grid-cols-2">
-          <img className="w-64" src="/hand.jpg" alt="hand" />
-          <img className="w-64" src="/gxfc.jpg" alt="hand" />
+    <div className="flex flex-col w-full mt-2 gap-2 p-2">
+      <Button className="w-full" onClick={async () => handleClicked("Hello, here is my most sincere greeting!")} >
+        Click me to get greeted</Button>
+        <div className="grid m:grid-cols-2 lg:grid-cols-4">
+          {[...Array(8)].map((x, i) =>
+          <img src={i+".jpg"} alt="gong xi fa cai" className="p-0.5 rounded" key={i} />
+  )}
         </div>
-        <Button className="w-full" onClick={async () => handleClicked("than kyou")} >
+        <Button className="w-full" onClick={async () => handleClicked("Than kyou! You have now been deeply thanked!")} >
           Click me to thank you</Button>
-          <div>Nice</div>
+          <div className="text-center opacity-30">TradAesthetics Inc. 2024</div>
     </div>
   );
 }
