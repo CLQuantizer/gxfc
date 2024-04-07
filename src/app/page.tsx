b2bc8a1a6f1374813dff3d7055b48e9e0fe874a6
eziox <ezioxxk@gmail.com>
2024-04-07 23:21:53 +0100

    jkhhkj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,14 @@ const randomGreetings = [
   "You are a wonderful person! (你是一个很棒的人!)",
   "Gong xi fa cai! (恭喜发财)",
   "Than kyou! You have now been deeply thanked! (谢谢!你现在已经被深深感谢了!)",
-  "May your day be filled with joy!(๑˃̵ᴗ˂ ̵)و (愿你的一天充满快乐!)",
+  "May your day be filled with joy!(愿你的一天充满快乐!)",
   "Hello, here is my most sincere greeting! (你好,这是我最真诚的问候!)",
   "Good luck! (祝你好运!)",
   "You are the best! (你是最棒的!)",
   "Hard work! (加油!)",
-  "You are the most beautiful! (你是最美丽的!)"
+  "You are the most beautiful! (你是最美丽的!)",
+  "Live in harmony! (和谐相处!)",
+  "The world is yours! (世界是你的!)",
 ]
 
 const currentYear = new Date().getFullYear();
@@ -30,7 +32,7 @@ export default function Home() {
       <Button className="w-full" onClick={async () => handleClicked("Hello, here is my most sincere greeting!")} >
         Click me to get greeted</Button>
         <div className="grid m:grid-cols-2 lg:grid-cols-4">
-          {[...Array(14)].map((x, i) => {
+          {[...Array(6)].map((x, i) => {
             // i%3!=0 or i<2
             if (i==1 || i==6) {
               return <img src={i+".jpg"} alt="gong xi fa cai" 
@@ -45,9 +47,39 @@ export default function Home() {
           )}
         })}
         </div>
+        <div className="container mx-auto mb-16">
+          <iframe className="w-full aspect-video" 
+                  src="https://www.youtube.com/embed/TWm31xbRVjI" 
+                  title="YouTube video player" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+          </iframe>
+      </div> 
+      <div className="grid m:grid-cols-2 lg:grid-cols-4">
+        {[7,8,9,10,11,12,13].map((x, i) => {
+              // i%3!=0 or i<2
+              if (x==1 || x==6) {
+                return <img src={x+".jpg"} alt="gong xi fa cai" 
+                onClick={async () => handleClicked(getRandomStringFromArray(randomGreetings))}
+                className="animate-slow-bounce backdrop-shadow
+                mt-12 hover:cursor-grab p-0.5 rounded" key={i} />
+            } else {
+              return (
+                <img src={x+".jpg"} alt={getRandomStringFromArray(randomGreetings)}
+                onClick={async () => handleClicked(getRandomStringFromArray(randomGreetings))}
+                className="hover:scale-105 hover:animate-slow-bounce hover:cursor-grab p-0.5 rounded-lg" key={i} />
+            )}
+          })}
+      </div>
+      <div className="container mx-auto mb-16">
+          <iframe className="w-full aspect-video" 
+                  src="https://www.youtube.com/embed/bu7nU9Mhpyo" 
+                  title="YouTube video player" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+          </iframe>
+      </div> 
         <Button className="w-full" onClick={async () => handleClicked("Than kyou! You have now been deeply thanked!")} >
           Click me to thank you</Button>
-          <div className="text-center opacity-30">TradAesthetics Inc. © {currentYear}</div>
+          <div className="text-center opacity-30">TradAesthetics (傳統美感) Inc. © {currentYear}</div>
     </div>
   );
 }
