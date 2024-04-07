b964cb5766c86386a4608971d9654141b516af68
eziox <ezioxxk@gmail.com>
2024-04-07 22:44:48 +0100

    kjkmn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,8 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "slow-spin": "spin 3s linear infinite",
+        "pulse-strong": "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite"
+        }
       },
     },
   },
